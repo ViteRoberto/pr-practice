854f72e5aaba161993ee9ed3d1293679370d5c15
robertocarlos.vite.contractor <robertocarlos.vite.contractor@bbva.com>
2023-03-21 10:59:52 -0600

    Adding multiplicación

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ let a = 23;
 let b = 32;
 
 let suma = a + b;
+let multiplicacion = a * b;
 
-console.log("Hola Mundo!");
+console.log("Suma: ",suma);
+console.log('Multiplicación: ',multiplicacion);
